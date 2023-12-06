df5f52fdcea15eabf02a3a82d610fc0661a384ed
Carlos J. Ortiz <ortizdcj@gmail.com>
2023-12-06 18:36:45 -0500

    add[Paging]: `IPagedList` interface

---
+++ src/Paging/IPagedList.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Qrtix.Paging;
+
+public interface IPagedList<out T> : IPager, IReadOnlyList<T>
+{
+	/// <summary>
+	/// One-based index of the first item in the paged subset, zero if the superset is empty or PageNumber
+	/// is greater than PageCount.
+	/// </summary>
+	/// <value>
+	/// One-based index of the first item in the paged subset, zero if the superset is empty or PageNumber
+	/// is greater than PageCount.
+	/// </value>
+	int FirstItemOnPage { get; }
+
+	/// <summary>
+	/// One-based index of the last item in the paged subset, zero if the superset is empty or PageNumber
+	/// is greater than PageCount.
+	/// </summary>
+	/// <value>
+	/// One-based index of the last item in the paged subset, zero if the superset is empty or PageNumber
+	/// is greater than PageCount.
+	/// </value>
+	int LastItemOnPage { get; }
+}
