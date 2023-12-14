375814b7602c4d706212aa33770c8e1cfd656730
Carlos J. Ortiz <ortizdcj@gmail.com>
2023-12-13 20:17:20 -0500

    feat: add movility to Pager class

--- src/Paging/Pagers/IPager.cs
+++ src/Paging/Pagers/IPager.cs
@@ -2,75 +2,90 @@
 
 public interface IPager
 {
+	#region Properties
+
 	/// <summary>
-    /// Total number of datasets within the data-source.
-    /// </summary>
-    /// <value>
-    /// Total number of datasets within the data-source.
-    /// </value>
-    int PageCount { get; }
+	/// Total number of datasets within the data-source.
+	/// </summary>
+	/// <value>
+	/// Total number of datasets within the data-source.
+	/// </value>
+	int PageCount { get; }
 
-    /// <summary>
-    /// Total number of objects contained within the datasets.
-    /// </summary>
-    /// <value>
-    /// Total number of objects contained within the data-source.
-    /// </value>
-    int TotalItemCount { get; }
+	/// <summary>
+	/// Total number of objects contained within the datasets.
+	/// </summary>
+	/// <value>
+	/// Total number of objects contained within the data-source.
+	/// </value>
+	int TotalItemCount { get; }
 
-    /// <summary>
-    /// One-based index of this subset within the data-source, zero if the data-source is empty.
-    /// </summary>
-    /// <value>
-    /// One-based index of this subset within the data-source, zero if the data-source is empty.
-    /// </value>
-    int PageNumber { get; }
+	/// <summary>
+	/// One-based index of this subset within the data-source, zero if the data-source is empty.
+	/// </summary>
+	/// <value>
+	/// One-based index of this subset within the data-source, zero if the data-source is empty.
+	/// </value>
+	int PageNumber { get; }
 
-    /// <summary>
-    /// Maximum size any individual subset.
-    /// </summary>
-    /// <value>
-    /// Maximum size any individual subset.
-    /// </value>
-    int PageSize { get; }
+	/// <summary>
+	/// Maximum size any individual subset.
+	/// </summary>
+	/// <value>
+	/// Maximum size any individual subset.
+	/// </value>
+	int PageSize { get; }
 
-    /// <summary>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is NOT the first subset within the data-source.
-    /// </summary>
-    /// <value>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is NOT the first subset within the data-source.
-    /// </value>
-    bool HasPreviousPage { get; }
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the first subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the first subset within the data-source.
+	/// </value>
+	bool HasPreviousPage { get; }
 
-    /// <summary>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is NOT the last subset within the data-source.
-    /// </summary>
-    /// <value>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is NOT the last subset within the data-source.
-    /// </value>
-    bool HasNextPage { get; }
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the last subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the last subset within the data-source.
+	/// </value>
+	bool HasNextPage { get; }
 
-    /// <summary>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the first subset within the data-source.
-    /// </summary>
-    /// <value>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the first subset within the data-source.
-    /// </value>
-    bool IsFirstPage { get; }
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the first subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the first subset within the data-source.
+	/// </value>
+	bool IsFirstPage { get; }
 
-    /// <summary>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the last subset within the data-source.
-    /// </summary>
-    /// <value>
-    /// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the last subset within the data-source.
-    /// </value>
-    bool IsLastPage { get; }
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the last subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the last subset within the data-source.
+	/// </value>
+	bool IsLastPage { get; }
+
+	#endregion
+
+	#region Methods
+
+	void Next(int movePagesForward = 1);
+	void Previous(int movePagesBackward = 1);
+	void First();
+	void Last();
+	void GoToPage(int pageNumber);
+	
+
+	#endregion
 }
--- src/Paging/Pagers/Pager.cs
+++ src/Paging/Pagers/Pager.cs
@@ -2,82 +2,6 @@
 
 public class Pager : IPager
 {
-	#region IPager Properties
-
-	/// <summary>
-	/// Total number of datasets within the data-source.
-	/// </summary>
-	/// <value>
-	/// Total number of datasets within the data-source.
-	/// </value>
-	public int PageCount { get; }
-	
-	/// <summary>
-	/// Total number of objects contained within the datasets.
-	/// </summary>
-	/// <value>
-	/// Total number of objects contained within the data-source.
-	/// </value>
-	public int TotalItemCount { get; }
-	
-	/// <summary>
-	/// One-based index of this subset within the data-source, zero if the data-source is empty.
-	/// </summary>
-	/// <value>
-	/// One-based index of this subset within the data-source, zero if the data-source is empty.
-	/// </value>
-	public int PageNumber { get; }
-	
-	/// <summary>
-	/// Maximum size any individual subset.
-	/// </summary>
-	/// <value>
-	/// Maximum size any individual subset.
-	/// </value>
-	public int PageSize { get; }
-	
-	/// <summary>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is NOT the first subset within the data-source.
-	/// </summary>
-	/// <value>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is NOT the first subset within the data-source.
-	/// </value>
-	public bool HasPreviousPage { get; }
-	
-	/// <summary>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is NOT the last subset within the data-source.
-	/// </summary>
-	/// <value>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is NOT the last subset within the data-source.
-	/// </value>
-	public bool HasNextPage { get; }
-	
-	/// <summary>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is the first subset within the data-source.
-	/// </summary>
-	/// <value>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is the first subset within the data-source.
-	/// </value>
-	public bool IsFirstPage { get; }
-	
-	/// <summary>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is the last subset within the data-source.
-	/// </summary>
-	/// <value>
-	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
-	/// is the last subset within the data-source.
-	/// </value>
-	public bool IsLastPage { get; }
-
-	#endregion
-	
 	#region Constructors
 
     public Pager(int pageNumber, int pageSize, int totalItemCount)
@@ -106,6 +30,114 @@ public class Pager : IPager
 		
 	}
 	
+	#endregion
+	
+	#region IPager Properties
+
+	/// <summary>
+	/// Total number of datasets within the data-source.
+	/// </summary>
+	/// <value>
+	/// Total number of datasets within the data-source.
+	/// </value>
+	public int PageCount { get; }
+	
+	/// <summary>
+	/// Total number of objects contained within the datasets.
+	/// </summary>
+	/// <value>
+	/// Total number of objects contained within the data-source.
+	/// </value>
+	public int TotalItemCount { get; }
+	
+	/// <summary>
+	/// One-based index of this subset within the data-source, zero if the data-source is empty.
+	/// </summary>
+	/// <value>
+	/// One-based index of this subset within the data-source, zero if the data-source is empty.
+	/// </value>
+	public int PageNumber { get; private set; }
+
+	/// <summary>
+	/// Maximum size any individual subset.
+	/// </summary>
+	/// <value>
+	/// Maximum size any individual subset.
+	/// </value>
+	public int PageSize { get; }
+	
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the first subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the first subset within the data-source.
+	/// </value>
+	public bool HasPreviousPage { get; private set; }
+
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the last subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is NOT the last subset within the data-source.
+	/// </value>
+	public bool HasNextPage { get; private set; }
+
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the first subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the first subset within the data-source.
+	/// </value>
+	public bool IsFirstPage { get; private set; }
+
+	/// <summary>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the last subset within the data-source.
+	/// </summary>
+	/// <value>
+	/// Returns true if the data-source is not empty and PageNumber is less than or equal to PageCount and this
+	/// is the last subset within the data-source.
+	/// </value>
+	public bool IsLastPage { get; private set; }
+
+	#endregion
+
+	#region IPager Methods
+
+	public void Next(int movePagesForward = 1)
+		=> GoToPage(PageNumber + movePagesForward);
+
+	public void Previous(int movePagesBackward = 1)
+		=> GoToPage(PageNumber - movePagesBackward);
+
+	public void First()
+		=> GoToPage(1);
+
+	public void Last()
+		=> GoToPage(PageCount);
+
+	public void GoToPage(int pageNumber)
+	{
+		var newPageNumber = pageNumber < 1 
+			? 1 
+			: pageNumber > PageCount
+				? PageCount
+				: pageNumber;
+
+		PageNumber = newPageNumber;
+		
+		HasPreviousPage = PageNumber > 1;
+		HasNextPage = PageNumber < PageCount;
+		IsFirstPage = PageNumber == 1;
+		IsLastPage =  PageNumber == PageCount;
+	}
+
 	#endregion
 
 	#region Private methods
