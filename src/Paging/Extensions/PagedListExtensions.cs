f62e7e55f92c5c558a68da3e9510657172fedb1b
Carlos J. Ortiz <ortizdcj@gmail.com>
2023-12-15 17:54:41 -0500

    add paged extensions methods

---
+++ src/Paging/Extensions/PagedListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Paging.PagedCollections;
+using Paging.Pagers;
+
+namespace Paging.Extensions;
+
+public static class PagedListExtensions
+{
+	public static PagedList<T> Paginated<T>(this IEnumerable<T> dataSource, int pageNumber, int pageSize) 
+		=> new(dataSource, pageNumber, pageSize);
+	
+	public static PagedList<T> Paginated<T>(this IEnumerable<T> dataSource, IPager pager)
+		=> new(dataSource, pager);
+	
+}
