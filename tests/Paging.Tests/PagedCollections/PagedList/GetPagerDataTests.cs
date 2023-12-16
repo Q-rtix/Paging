0c4f78369819cabcebb0a75f6f95cb51eda8aaf7
Carlos J. Ortiz <ortizdcj@gmail.com>
2023-12-15 19:53:49 -0500

    add indexing and GetPagerData tests

---
+++ tests/Paging.Tests/PagedCollections/PagedList/GetPagerDataTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Paging.PagedCollections;
+
+namespace Paging.Tests.PagedCollections.PagedList;
+
+public class GetPagerDataTests
+{
+	[Fact]
+	public void GetPagerData()
+	{
+		// Arrange
+		var pager = Lab.DataSources.Pagers.PageNumberFirst;
+		var pagedList = new PagedList<int>(
+			Lab.DataSources.IntegerLists.Items50,
+			pager
+		);
+		
+		// Act
+		var newPager = pagedList.GetPagerData();
+		
+		// Assert
+		newPager.IsFirstPage.Should().Be(pager.IsFirstPage);
+		newPager.IsLastPage.Should().Be(pager.IsLastPage);
+		newPager.PageNumber.Should().Be(pager.PageNumber);
+		newPager.PageSize.Should().Be(pager.PageSize);
+		newPager.TotalItemCount.Should().Be(pager.TotalItemCount);
+		newPager.PageCount.Should().Be(pager.PageCount);
+		newPager.HasPreviousPage.Should().Be(pager.HasPreviousPage);
+		newPager.HasNextPage.Should().Be(pager.HasNextPage);
+	}
+}
---
+++ tests/Paging.Tests/PagedCollections/PagedList/ThisIndexTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Paging.Extensions;
+
+namespace Paging.Tests.PagedCollections.PagedList;
+
+public class ThisIndexTests
+{
+	[Theory]
+	[MemberData(nameof(ValidIndexes))]
+	public void DirectIndexing_WithValidIndexes_ReturnsTheItemIndexed(int index)
+	{
+		// Arrange 
+		var list = Lab.DataSources.IntegerLists.Items50;
+		var pagedList = list.Paginated(Lab.DataSources.Pagers.PageNumberFirst);
+		
+		// Act
+		var item = pagedList[index];
+		
+		// Assert
+		item.Should().Be(list[index]);
+	}
+	
+	[Theory]
+	[MemberData(nameof(InvalidIndexes))]
+	public void DirectIndexing(int index)
+	{
+		// Arrange 
+		var pagedList = Lab.DataSources.IntegerLists.Items50.Paginated(Lab.DataSources.Pagers.PageNumberFirst);
+		
+		// Act
+		var act = () =>
+		{
+			var item = pagedList[index];
+		};
+		
+		// Assert
+		act.Should().Throw<ArgumentOutOfRangeException>();
+	}
+
+	public static IEnumerable<object[]> ValidIndexes => new List<object[]>
+	{
+		new object[] { 0 },
+		new object[] { 3 },
+		new object[] { Lab.DataSources.Pagers.PageNumberFirst.PageCount },
+	};
+	
+	public static IEnumerable<object[]> InvalidIndexes => new List<object[]>
+	{
+		new object[] { -1 },
+		new object[] { Lab.DataSources.Pagers.PageNumberFirst.PageCount + 5 }
+	};
+}
